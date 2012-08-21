f764dea9b1a7c4cb489fdc33b3465e771af9582e
Rpsl <im.vitman@gmail.com>
2012-08-21 16:26:23 +0400

    small refactor

--- config.php
+++ config.php
@@ -4,19 +4,21 @@
 	include_once( realpath( __DIR__ ) . '/includes/class.phpmailer-lite.php');
 
 	TurboFilm::$config = array(
-		'login'			=> 'name',
+		'login'			=> 'login',
 		'password'		=> 'password',
 		'cookie_file'	=> realpath(__DIR__) . '/cookie.txt',
 		'tools'         => array(
 			'mkdir'     => '/bin/mkdir',
-			'wget'      => '/usr/bin/wget'
+			'wget'      => '/usr/bin/wget',
+			'chmod'     => '/bin/chmod'
 		),
 		'watch'         => 1,
 		'language'      => 'ru', // ru | en
 		'only_hq'       => FALSE,
 		'download_all'  => TRUE,
 		'tasks'			=> 999,
-		'email'         => array('email'),
+		'email'         => array('mail@me.com'),
 		'download_dir'  => '/c/media/Turbofilm',
 		'log_file'      => '/c/media/Turbofilm/downloads.log',
 	);
+
