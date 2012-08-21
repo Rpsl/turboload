02d75e65f29f2a9c186adffe7b669f13c16f9121
Rpsl <im.vitman@gmail.com>
2012-08-21 22:06:38 +0400

    config.php

--- config.php
+++ config.php
@@ -1,22 +1,36 @@
 <?php
 
-	include_once( realpath( __DIR__ ) . '/includes/simple_html_dom.php');
-	include_once( realpath( __DIR__ ) . '/includes/class.phpmailer-lite.php');
+	/*
+		login       - Ваш логин
+		password    - Ваш пароль
+		watch       - Отмечать серию как просмотренную после скачивания
+		language    - Язык на котором скачивать серию
+		tasks       - Максимальное кол-во серий скачиваемых за раз
+		email       - email для уведомлений
+		download_dir- Путь к папке для загрузок
+
+		cookie_file - путь к cookie файлу, лучше не трогайте
+		tools       - масив с путями к бинарникам
+		only_hq     - Скачивать только в высоком качестве
+				по умолчанию всегда выбирается наивысшее качество.
+				если установить в TRUE, то серии не в HQ скачиваться не будут
+	 */
 
 	TurboFilm::$config = array(
 		'login'			=> 'login',
 		'password'		=> 'password',
+		'watch'         => 1,
+		'language'      => 'ru', // ru | en
+		'tasks'			=> 5,
+		'email'         => array('mail@me.com'),
+		'download_dir' 	=> realpath( __DIR__ ) . '/downloads',
+
 		'cookie_file'	=> realpath(__DIR__) . '/cookie.txt',
 		'tools'         => array(
 			'wget'      => '/usr/bin/wget'
 		),
-		'watch'         => 1,
-		'language'      => 'ru', // ru | en
 		'only_hq'       => FALSE,
-		'download_all'  => TRUE,
-		'tasks'			=> 5,
-		'email'         => array('mail@me.com'),
-		'download_dir' 	=> realpath( __DIR__ ) . '/downloads',
+
 		'log_file' 		=> realpath( __DIR__ ) . '/downloads.log',
 	);
 
